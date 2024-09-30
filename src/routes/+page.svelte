57e89bdb608c4fbc7d187470a02d89861bc18731
Artur_Karakashyan <atupatu2005@mail.ru>
2024-09-30 19:16:51 +0300

    omg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Lorem ipsum dolor <strong>пизда</strong> sit amet consectetur adipisicing elit. Reiciendis aperiam
+	at tempore voluptate cumque eum optio expedita dolor repellendus, fugiat porro quos. Tempore et veritatis
+	architecto fugit ullam id culpa.
+</p>
+<h2>hui</h2>
+<h3>govno</h3>
+<h4>mudila</h4>
+<h5>rukoblud</h5>
+<h6>ssanina</h6>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quibusdam expedita laudantium modi
+	nesciunt? Aspernatur doloremque pariatur earum excepturi explicabo, placeat rem velit? Quaerat
+	optio asperiores ducimus ab aliquam! Quis.
+</p>
