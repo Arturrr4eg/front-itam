4bb0bca284997693c56c0abcc386a649df475dcb
Artur_Karakashyan <atupatu2005@mail.ru>
2024-10-15 22:29:07 +0300

    ok

---
+++ src/components/Image.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let size = 150;
+</script>
+
+<img src="/cover.png" alt="itam" />
+<p>hello{size}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>
-	Lorem ipsum dolor <strong>пизда</strong> sit amet consectetur adipisicing elit. Reiciendis aperiam
-	at tempore voluptate cumque eum optio expedita dolor repellendus, fugiat porro quos. Tempore et veritatis
-	architecto fugit ullam id culpa.
-</p>
-<h2>hui</h2>
-<h3>govno</h3>
-<h4>mudila</h4>
-<h5>rukoblud</h5>
-<h6>ssanina</h6>
-<p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quibusdam expedita laudantium modi
-	nesciunt? Aspernatur doloremque pariatur earum excepturi explicabo, placeat rem velit? Quaerat
-	optio asperiores ducimus ab aliquam! Quis.
-</p>
+<script lang="js">
+	import Image from "../components/Image.svelte";
+
+	let number1 = 0;
+	let number2 = 0;
+</script>
+
+<input bind:value={number1} type="number"/>
+<input bind:value={number2} type="number"/>
+
+<p>{number1} + {number2} = {number1+number2}</p>
+
+
+
+
+<!-- {#if number == 10}
+	<p>{number}</p>
+{:else}
+	<Image size={25} />
+{/if}
+
+<Image size={150} /> -->
